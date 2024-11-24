c65c8614e6b17a3e9b4b4cd53e9adad2d5e919df
farith <farithadhil@gmail.com>
2024-11-24 12:32:49 +0530

    canvas error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+      config.resolve.alias.canvas = false;
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        canvas: false,
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
